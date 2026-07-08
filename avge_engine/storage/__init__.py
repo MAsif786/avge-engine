b306df7c127fe7655878a8e1037d5754ac716d1b
Muhammad Asif <snew3110@gmail.com>
2026-07-09 01:25:07 +0500

    Add storage __init__.py (restored after cleanup)

---
+++ avge_engine/storage/__init__.py
@@ -0,0 +1,6 @@
+"""Storage adapters for scene graph persistence."""
+
+from avge_engine.storage.adapter import StorageAdapter
+from avge_engine.storage.file_adapter import FileStorageAdapter
+
+__all__ = ["StorageAdapter", "FileStorageAdapter"]
